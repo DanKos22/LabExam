adb1974a695a000d8c4c93043622c29fa82eb79f
Dan Koskiranta <G00397054@atu.ie>
2022-12-12 09:26:30 +0000

    Details for Shiftworker class

---
+++ src/main/java/ie/atu/week12/ShiftWorker.java
@@ -0,0 +1,37 @@
+package ie.atu.week12;
+
+public class ShiftWorker extends Employee {
+
+    private int shiftNum;
+    private int payrollNum;
+    private double hourly_pay_rate;
+
+    public ShiftWorker() {
+    }
+
+    public int getShiftNum() {
+        return shiftNum;
+    }
+
+    public void setShiftNum(int shiftNum) {
+        this.shiftNum = shiftNum;
+    }
+
+    public int getPayrollNum() {
+        return payrollNum;
+    }
+
+    public void setPayrollNum(int payrollNum) {
+        this.payrollNum = payrollNum;
+    }
+
+    public double getHourly_pay_rate() {
+        return hourly_pay_rate;
+    }
+
+    public void setHourly_pay_rate(double hourly_pay_rate) {
+        this.hourly_pay_rate = hourly_pay_rate;
+    }
+
+    void getReportDetails(String name, int payrollNum, int shiftNum, double hourly_pay_rate)
+}
